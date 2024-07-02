d159b19df58cd1992a353cb0d38bb50b9926c344
KaPuChiNo <107169377+69KaPuChiNo@users.noreply.github.com>
2024-07-02 11:57:45 -0700

    Update metaghost.py

--- metaghost.py
+++ metaghost.py
@@ -2,12 +2,8 @@
 # Copyright (c) 2024 69KaPuChiNo
 # Licensed under the MIT License
 
-
 # Author - 69KaPuChiNo
-
 # github - https://github.com/69KaPuChiNo
-
-
 
 import os
 import sys
@@ -18,18 +14,16 @@ import webbrowser
 
 class MetaDataFinder:
     def __init__(self):
-        
         self.R = '\033[91m'  
         self.Y = '\033[93m'  
         self.G = '\033[92m'  
         self.CY = '\033[96m'  
         self.W = '\033[97m'  
 
-        
         self.image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+        self.common_paths = ['~/Downloads', '~/Desktop', '~/Pictures', '~/Videos', '~/Documents', '~/Music']
 
     def start(self):
-        
         os.system('clear')
         print(self.CY + """
 ███╗   ███╗███████╗████████╗ █████╗      ██████╗ ██╗  ██╗ ██████╗ ███████╗████████╗
@@ -37,7 +31,7 @@ class MetaDataFinder:
 ██╔████╔██║█████╗     ██║   ███████║    ██║  ███╗███████║██║   ██║███████╗   ██║   
 ██║╚██╔╝██║██╔══╝     ██║   ██╔══██║    ██║   ██║██╔══██║██║   ██║╚════██║   ██║   
 ██║ ╚═╝ ██║███████╗   ██║   ██║  ██║    ╚██████╔╝██║  ██║╚██████╔╝███████║   ██║   
-╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝     ╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝                                                                                   
+╚═╝     ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝     ╚═════╝ ╚═╝  ██╔╝╚═════╝ ╚══════╝   ╚═╝                                                                                   
 """ + self.Y + """ v1.0""" + self.G + """
         
          Comprehensive Image Metadata Finder 
@@ -136,8 +130,19 @@ class MetaDataFinder:
         print(self.G + "Metadata saved as JSON file." + self.W)
 
     def check_image_metadata(self):
-        self.list_photos()
+        self.list_common_paths()
         try:
+            path_choice = int(input(self.CY + "\nEnter the number of the path (0 to enter custom path): " + self.W))
+            if path_choice == 0:
+                custom_path = input(self.CY + "Enter the custom path: " + self.W)
+                self.list_photos(custom_path)
+            else:
+                selected_path = self.select_path(path_choice)
+                if selected_path:
+                    self.list_photos(selected_path)
+                else:
+                    print(self.R + "\nInvalid choice! Please try again.\n")
+                    self.check_image_metadata()
             choice = int(input(self.CY + "\nEnter the number of the image to view metadata (0 to cancel): " + self.W))
             if choice == 0:
                 return
@@ -153,7 +158,6 @@ class MetaDataFinder:
                     self.display_metadata(metadata)
                     self.save_metadata(metadata)
 
-                    
                     self.show_location_on_maps(exif_data)
                 else:
                     print(self.R + "\nInvalid choice! Please try again.\n")
@@ -163,11 +167,8 @@ class MetaDataFinder:
             self.check_image_metadata()
 
     def select_image(self, choice):
-        files = os.listdir('.')
-        image_files = [f for f in files if os.path.isfile(f) and any(f.lower().endswith(ext) for ext in self.image_extensions)]
-
-        if 1 <= choice <= len(image_files):
-            return image_files[choice - 1]
+        if 1 <= choice <= len(self.image_files):
+            return self.image_files[choice - 1]
         else:
             return None
 
@@ -178,47 +179,56 @@ class MetaDataFinder:
             google_maps_url = f"https://www.google.com/maps?q={lat},{lon}"
             print(self.G + f"GPS Coordinates : {lat}, {lon}")
             print(self.Y + f"Google Maps Link : {google_maps_url}")
-            
         else:
             print(self.R + "No GPS coordinates found in metadata")
 
     def get_gps_coordinates(self, exif_data):
         if 'GPS GPSLatitude' in exif_data and 'GPS GPSLongitude' in exif_data:
-            lat = exif_data['GPS GPSLatitude']
-            lon = exif_data['GPS GPSLongitude']
-            lat_ref = exif_data.get('GPS GPSLatitudeRef', {}).values
-            lon_ref = exif_data.get('GPS GPSLongitudeRef', {}).values
-
-            lat = self.convert_to_degrees(lat)
-            lon = self.convert_to_degrees(lon)
-
-            if lat_ref and lat_ref != "N":
+            lat = self.convert_to_degrees(exif_data['GPS GPSLatitude'].values)
+            lon = self.convert_to_degrees(exif_data['GPS GPSLongitude'].values)
+            if exif_data['GPS GPSLatitudeRef'].values[0] != 'N':
                 lat = -lat
-            if lon_ref and lon_ref != "E":
+            if exif_data['GPS GPSLongitudeRef'].values[0] != 'E':
                 lon = -lon
-
             return lat, lon
-        else:
-            print(self.R + "GPS coordinates not found in metadata")
-            return None
+        return None
 
     def convert_to_degrees(self, value):
-        d = float(value.values[0].num) / float(value.values[0].den)
-        m = float(value.values[1].num) / float(value.values[1].den)
-        s = float(value.values[2].num) / float(value.values[2].den)
-
+        d = float(value[0].num) / float(value[0].den)
+        m = float(value[1].num) / float(value[1].den)
+        s = float(value[2].num) / float(value[2].den)
         return d + (m / 60.0) + (s / 3600.0)
 
     def remove_metadata(self):
-        self.list_photos()
+        self.list_common_paths()
         try:
+            path_choice = int(input(self.CY + "\nEnter the number of the path (0 to enter custom path): " + self.W))
+            if path_choice == 0:
+                custom_path = input(self.CY + "Enter the custom path: " + self.W)
+                self.list_photos(custom_path)
+            else:
+                selected_path = self.select_path(path_choice)
+                if selected_path:
+                    self.list_photos(selected_path)
+                else:
+                    print(self.R + "\nInvalid choice! Please try again.\n")
+                    self.remove_metadata()
             choice = int(input(self.CY + "\nEnter the number of the image to remove metadata (0 to cancel): " + self.W))
             if choice == 0:
                 return
             else:
                 selected_image = self.select_image(choice)
                 if selected_image:
-                    self.remove_exif_metadata(selected_image)
+                    try:
+                        image = Image.open(selected_image)
+                        data = list(image.getdata())
+                        image_without_exif = Image.new(image.mode, image.size)
+                        image_without_exif.putdata(data)
+                        new_image_path = os.path.splitext(selected_image)[0] + "_no_metadata" + os.path.splitext(selected_image)[1]
+                        image_without_exif.save(new_image_path)
+                        print(self.G + "Metadata removed successfully. New image saved as " + new_image_path + self.W)
+                    except Exception as e:
+                        print(self.R + f"An error occurred: {str(e)}" + self.W)
                 else:
                     print(self.R + "\nInvalid choice! Please try again.\n")
                     self.remove_metadata()
@@ -226,105 +236,42 @@ class MetaDataFinder:
             print(self.R + "\nInvalid input! Please enter a number.\n")
             self.remove_metadata()
 
-    def remove_exif_metadata(self, image_path):
-        try:
-            image = Image.open(image_path)
-            image_without_exif = Image.new(image.mode, image.size)
-            image_without_exif.paste(image)
+    def list_common_paths(self):
+        print(self.Y + "\nCommon Paths:")
+        for idx, path in enumerate(self.common_paths, start=1):
+            print(f"{idx}) {os.path.expanduser(path)}")
+        print("0) Enter custom path")
 
-            new_file_name = input(self.G + "\n>>> " + self.Y + "Enter new file name for the image without metadata:" + self.W + " ")
-            image_without_exif.save(new_file_name + image_path[-4:])
-            print(self.G + "Metadata removed and saved as " + new_file_name + image_path[-4:] + self.W)
+    def select_path(self, choice):
+        if 1 <= choice <= len(self.common_paths):
+            return os.path.expanduser(self.common_paths[choice - 1])
+        else:
+            return None
+
+    def list_photos(self, directory):
+        self.image_files = []
+        print(self.Y + f"\nImages in {directory}:")
+        try:
+            files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+            for file in files:
+                if file.lower().endswith(tuple(self.image_extensions)):
+                    self.image_files.append(os.path.join(directory, file))
+            if not self.image_files:
+                print(self.R + "No images found in this directory.")
+            else:
+                for idx, file in enumerate(self.image_files, start=1):
+                    print(f"{idx}) {file}")
         except Exception as e:
-            print(self.R + f"Error: {e}")
-
-    def list_photos(self):
-        files = os.listdir('.')
-        image_files = [f for f in files if os.path.isfile(f) and any(f.lower().endswith(ext) for ext in self.image_extensions)]
-
-        print(self.R + "\n==============================================================")
-        print(self.Y + '\n>>> ' + self.CY + 'List of Photos in Your Device\n')
-        for idx, image in enumerate(image_files):
-            print(self.G + f"{idx + 1}) " + self.Y + image)
-        print(self.R + "\n==============================================================")
-
-    def run(self):
-        try:
-            self.start()
-            self.menu()
-        except KeyboardInterrupt:
-            print(self.Y + "\nInterrupted! Have a nice day :)" + self.W)
+            print(self.R + f"An error occurred while listing photos: {str(e)}")
 
     def handle_exit_choice(self):
-        try:
-            print(self.Y + """
-                                                                                           +@+ .       ..*@+                                         
-                                                                                           -@*-@@.  .  .@@:#@.                                        
-                                                                                           *@-%@- -@@@: +@* @-                                        
-                                                                                           =@=#@+ :%@%. *@**@:                                        
-                                                                                            #@+%#  *@* .@*+@#                                         
-             ..      ...      ...       .......      ..       ..        .:--:.   ..        ..=+    *@*   .#*.       ..      .:-+++-:                  
-             @@    .#@*.     +@@@.      @@%###@%+   =@*       @@-    -#@%#####+  @@*       @@      *@*    *@@-      #@-   -+%*. *.=%++.               
-             @@   =@%-      .@@+@#      @@=    *@+  =@*       @@-   #@*.         @@*       @@      *@*    *@%@+     *@- .#*+#--+%--#%+@=              
-             @@ -@@-        #@: %@-     @@=    :@*  =@*       @@-  @@=           @@*       @@      *@*    *@-+@%.   *@-.%. #    *   :* ==             
-             @@#@@#        +@+   %@     @@*---*@@.  =@*       @@- -@@            @@@@@@@@@@@@      *@*    *@- :@@=  *@--#+#%++++%++++@++@             
-             @@=.=@#      :@@*+++%@#    @@#+++=.    =@*       @@- :@@.           @@+       @@      *@*    *@-  .%@+ *@--* -*   :*    #  %             
-             @@   -@@-    %@*-----@@=   @@=         .@#      .@@   #@*           @@*       @@      *@*    *@-    #@+*@- #-.#...-#...+*.#-             
-             @@    :%@+  *@%      -@@.  @@=          +@*.   :%@+    +@%+:    ..  @@*      :@@      *@*    *@-     *@@@-  +#+@--+#..+#-#:              
-             **      +*+:**        =*+  **-           -+#%%#*=       .=**%%%#*-  **-       #*      =*=    =*:      -**:    =*#+*#=##=-                
-                                                                                             
-            """ + self.W)
-            sys.exit(0)
-        except KeyboardInterrupt:
-            print(self.Y + "\nInterrupted! Have a nice day :)" + self.W)
+        print(self.G + "\nThank you for using MetaGhost. Goodbye!")
+        sys.exit(0)
 
     def handle_no_save_choice(self):
-        try:
-            print(self.Y + "\nDo you want to:")
-            print(self.G + "1) Open GPS on Google Maps")
-            print(self.G + "2) Exit")
-            choice = int(input(self.CY + "Enter Your choice: " + self.W))
-            if choice == 1:
-                self.open_gps_on_maps()
-            elif choice == 2:
-                self.handle_exit_choice()
-            else:
-                print(self.R + "\nInvalid choice! Please try again\n")
-                self.handle_no_save_choice()
-        except ValueError:
-            print(self.R + "\nInvalid choice! Please try again\n")
-            self.handle_no_save_choice()
-
-    def open_gps_on_maps(self):
-        try:
-            self.list_photos()
-            choice = int(input(self.CY + "\nEnter the number of the image to open GPS coordinates on Google Maps (0 to cancel): " + self.W))
-            if choice == 0:
-                return
-            else:
-                selected_image = self.select_image(choice)
-                if selected_image:
-                    exif_data = self.get_exif_data(selected_image)
-                    if not exif_data:
-                        print(self.R + "No EXIF data found")
-                        return
-
-                    gps_coordinates = self.get_gps_coordinates(exif_data)
-                    if gps_coordinates:
-                        lat, lon = gps_coordinates
-                        google_maps_url = f"https://www.google.com/maps?q={lat},{lon}"
-                        print(self.G + f"Opening GPS Coordinates : {lat}, {lon}")
-                        print(self.Y + f"Opening Google Maps Link : {google_maps_url}")
-                        webbrowser.open(google_maps_url, new=2)
-                    else:
-                        print(self.R + "No GPS coordinates found in metadata")
-                else:
-                    print(self.R + "\nInvalid choice! Please try again.\n")
-                    self.open_gps_on_maps()
-        except ValueError:
-            print(self.R + "\nInvalid input! Please enter a number.\n")
-            self.open_gps_on_maps()
+        print(self.G + "Metadata not saved." + self.W)
 
 if __name__ == "__main__":
-    metadata_finder = MetaDataFinder()
-    metadata_finder.run()
+    app = MetaDataFinder()
+    app.start()
+    app.menu()
